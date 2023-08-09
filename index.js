a4c98993b1ad7dcb36ddaf9b1ea83e738867191d
rune-encoder <132966351+rune-encoder@users.noreply.github.com>
2023-08-09 03:34:08 -0500

    1. Completed Write File Function: SVG file generated, 2. Improved color list, 3. Fixed text not showing up on generated SVG. 4. Refactored code

--- index.js
+++ index.js
@@ -1,44 +1,53 @@
-const inquirer = require('inquirer');
-const { writeFile } = require('fs').promises;
+const inquirer = require("inquirer");
+const { writeFile } = require("fs").promises;
 
 const renderColorList = require("./lib/renderColorList.js");
-const generateLogo = require('./lib/generateLogo.js')
+const generateLogo = require("./lib/generateLogo.js");
 const cliColors = require("./lib/console-colors.js");
 
 const questions = () => {
-    return inquirer.prompt([
-      {
-        type: "input",
-        name: "text",
-        message: `${cliColors.magenta(`What is your logo's text? (Type up to three characters.)`)}`,
-      },
-      {
-        type: "list",
-        name: "textColor",
-        message: `${cliColors.cyan(`What is the color of your logo's text?`)}`,
-        choices: renderColorList.prototype.colorList,
-      },
-      {
-        type: "list",
-        name: "logoShape",
-        message: `${cliColors.blue(`Choose your logo's shape:`)}`,
-        choices: ['Circle', 'Square', 'Triangle'],
-      },
-      {
-        type: "list",
-        name: "logoColor",
-        message: `${cliColors.red(`What is the color of your logo's shape?`)}`,
-        choices: renderColorList.prototype.colorList,
-      },
-    ]);
-  };
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "text",
+      message: `${cliColors.green(
+        `What is your logo's text? (Type up to three characters.)`
+      )}`,
+    },
+    {
+      type: "list",
+      name: "textColor",
+      message: `${cliColors.cyan(`What is the color of your logo's text?`)}`,
+      choices: renderColorList.prototype.colorList,
+    },
+    {
+      type: "list",
+      name: "logoShape",
+      message: `${cliColors.green(`Choose your logo's shape:`)}`,
+      choices: ["Circle", "Square", "Triangle"],
+    },
+    {
+      type: "list",
+      name: "logoColor",
+      message: `${cliColors.cyan(`What is the color of your logo's shape?`)}`,
+      choices: renderColorList.prototype.colorList,
+    },
+  ]);
+};
 
-  const init = () => {
-    renderColorList()
-    questions()
-    .then((res) => generateLogo(res))
+const init = () => {
+  renderColorList();
+  questions()
+    .then((response) => generateLogo(response))
+    .then((response) => {
+      const filename = `${generateLogo.prototype.something.text
+        .toLowerCase()
+        .split(" ")
+        .join("")}.svg`;
+      writeFile(filename, response);
+    })
     .then(() => console.log("Successful"))
     .catch((err) => console.log(err));
-  }
+};
 
-  init();
+init();
